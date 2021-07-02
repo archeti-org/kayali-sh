bdc430b22f546e52d109c2f5ba3e6a44b0ccec95
Kent Kawahara <kak@odoo.com>
2021-07-02 15:16:50 -0700

    [ADD] kayali_account_stock: Add invoice and barcode customization

---
+++ kayali_account_stock/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Societe Kayali International : Customisation for customer invoice and ',
+    'summary': 'Customisation for customer invoice',
+    'sequence': 100,
+    'license': 'OEEL-1',
+    'website': 'https://www.odoo.com',
+    'version': '1.1',
+    'author': 'Odoo Inc',
+    'description': """
+        Task ID: 2327984
+        - Sort invoice lines by description
+        - Stop edit from going to the top of the page
+    """,
+    'category': 'Custom Development',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account', 'stock_barcode'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
